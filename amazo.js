5a19a30ce7e94ba8924cc55b1069b8d3f44a83d9
kuttycop <kuttycop562003@gmail.com>
2024-08-23 10:19:52 +0530

    Amazone clone files added

---
+++ amazo.js
@@ -0,0 +1,26 @@
+// let index = 0;
+// function showSlide(n) {
+//     const slides = document.querySelector('.slides');
+//     if (n >= slides.children.length) index = 0;
+//     if (n < 0) index = slides.children.length - 1;
+//     slides.style.transform = `translateX(${-index * 100}%)`;
+// }
+// // Optional: Auto-slide functionality
+// setInterval(() => {
+//     index++;
+//     showSlide(index);
+// }, 10000);
+// showSlide(index);
+
+// let index1=0;
+// function showSlide1(n){
+//     const slideses=document.querySelector('.slides1');
+//     if (n >= slideses.children.length) index = 0;
+//     if (n < 0) index = slideses.children.length - 1;
+//     slideses.style.transform = `translateX(${-index * 100}%)`;
+// }
+// setInterval(()=>{
+//     index1++;
+//     showSlide1(index1);
+// },15000);
+// showSlide1(index1);
